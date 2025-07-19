d84581fea451b33b9a80f9cd693f72e4b8f2569c
Gitik Raj Jindal <118770597+gitikraj@users.noreply.github.com>
2025-07-19 16:24:46 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
 
             <Link href="/" className="flex items-center gap-2">
                 <Image
-                    src="/logo.png"
+                    src="/Logos/logo.png"
                     alt="Logo"
                     width={150}
                     height={60}
